54cae53b3b5eba25bfa09e5281d42d286d2dab89
nyccreator <andyfernandeznyc@gmail.com>
2024-10-19 19:06:25 -0400

    Initial Commit

---
+++ HooperTests/HooperTests.swift
@@ -0,0 +1,17 @@
+//
+//  HooperTests.swift
+//  HooperTests
+//
+//  Created by Andrey Fernandez on 10/19/24.
+//
+
+import Testing
+@testable import Hooper
+
+struct HooperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
